bf0be4ee3649c469e67c0e65d6e30396f9a968e0
shinminho <shinminho@gmail.com>
2018-08-29 21:33:33 +0900

    Update Bidder.sol

--- Bidder.sol
+++ Bidder.sol
@@ -1,50 +1,42 @@
 pragma solidity ^0.4.24;
 
 import "./Mortal.sol";
+//import "./Bazaar.sol";
 
-// This contract is used for BazaarAuction application
-// Slight changes are made to AuctionV2 contract
-contract Auction is Mortal {
+interface Bazaar {
+    function register() external returns (address[]);
+}
 
-	uint public highestBid = 0;
-	address public highestBidder;
-	bool isOpen = true;
+interface Auction {
+    function bid() external;
+    function withdraw( address ) external;
+}
+
+contract Bidder is Mortal {
     
-    mapping (address=>uint) moneyback;
+    address bazaar;
+    address[] items;
     
-	modifier ifOpen { require( isOpen ); _; }
+    constructor(address bazaar_) public {
+       bazaar = bazaar_;
+       //register
+       items = Bazaar(bazaar).register();
+        //address[] storage items = bazaar.call("register");
+    } 
+    
+    function bid(uint index, uint amount) public {
+       //Auction(items[index]).bid();
+       if(!items[index].call.value(amount)("bid()"))
+           revert("calling bid() of Auction failed");
+    }
 
-	function bid() public payable ifOpen {
-	    require( msg.value > highestBid );
-		if( highestBid != 0 ) 
-		    // highestBidder.transfer( highestBid ); // Dangerous for DoS
-		    moneyback[highestBidder] += highestBid;
-		highestBid = msg.value;
-		highestBidder = msg.sender; 
-	}
-
-    function withdraw(address beneficiary) public payable {
-        //check
-        require( moneyback[msg.sender] > 0 );
+    function result() public {
         
-        //effect
-        uint amount = moneyback[msg.sender];
-        moneyback[msg.sender] = 0;
-        
-        //interact
-        beneficiary.transfer( amount );
-    }	
-    
-    function open() public onlyOwner {
-        isOpen = true;
     }
     
-	function close(address beneficiary) public payable onlyOwner ifOpen { 
-	    isOpen=false; 
-	    beneficiary.transfer( highestBid );
-	}
-	
-	function winner() public view returns (address) { 
-	    return highestBidder; 
-	}
+    function withdraw() public {
+        for( uint i=0; i<items.length; i++ ) {
+            Auction(items[i]).withdraw(owner);
+        }
+    }
 }
